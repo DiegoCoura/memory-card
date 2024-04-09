bf1f6ae62a3e8ad281506cb240b88ed4e7589304
DiegoCoura <diegogomes.10.9@gmail.com>
2024-04-09 19:38:56 -0300

    refactor: rename StartGame with StartPage

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,10 +1,10 @@
-import "../styles/startgame.css"
+import "../styles/startpage.css"
 import sharinganIMG from "../assets/imgs/sharingan.png";
 import narutoIcon from '../assets/imgs/happy-naruto.png'
 
-export default function StartGame({ startGame }) {
+export default function StartPage({ startGame }) {
   return (
-    <div className="start-game">
+    <div className="start-page">
     <div className="naruto-container">
         <img src={narutoIcon} className="happy-naruto" alt="happy naruto" />
     </div>
