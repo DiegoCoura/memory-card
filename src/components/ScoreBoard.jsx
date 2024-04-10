52a0498102a5f99887209ba95b8a6c5688481dbe
DiegoCoura <diegogomes.10.9@gmail.com>
2024-04-10 16:12:26 -0300

    feat: change the scoreboard img source

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,10 +1,11 @@
 import '../styles/scoreboard.css'
+import narutoScroll from "../assets/imgs/naruto-scroll.png"
 
 export default function ScoreBoard ({ topScore, currScore }){
     return(
         <>
             <div className="scoreboard">
-                <img src="https://www.nicepng.com/png/full/419-4199514_alterar-exibio-de-spoiler-para-tipo-pergaminho-naruto.png" className="scoreboard-bg" alt="naruto scroll" />
+                <img src={narutoScroll} className="scoreboard-bg" alt="naruto scroll" />
                 <p>Record: {topScore}</p>
                 <p>Score: {currScore}</p>
             </div>
